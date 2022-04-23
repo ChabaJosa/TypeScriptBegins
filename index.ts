cc7963d665db0ef5618d6640e91be33b0f4926b8
ChabaJosa <chabagjg@gmail.com>
2022-04-23 10:32:17 -0400

    Organized comments

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
-// Following https://www.youtube.com/watch?v=BCg4U1FzODs
+// Following --> https://www.youtube.com/watch?v=BCg4U1FzODs
+// TS Docs   --> https://www.typescriptlang.org/docs/
 //
-let id = 5;         // TS infers the type as if you were doing ===>  id: number = 5
-let tsID: any = 5;  // Any removes the static type requirement
+//  Requirements:
+//      -Requires Global Install of TS
+//      -To run with TS compiler use `tsc index.ts` instead of `node index.ts`
 //
-// id = 'Hi'           // Since the number type was inferred, then this won't compile
-tsID = '5'
+//  Notes:
+//      -IF you run `tsc --watch index.ts` it will check for errors on every saved change 
+//       and give you input on the terminal
+//
+//  Examples:
+//
+let id = 5;    
+//  -TS infers the type as if you were doing ===>  id: number = 5
+//  -If you tried setting id = 'Hi'  -->  Since the number type was inferred, then this won't compile
+//
+let tsID: any = 5;  
+tsID = '5' 
+//  -Any removes the static type requirement, you could also do number, string, boolean, array, etc
+//
 
-console.log(id, tsID)
 
